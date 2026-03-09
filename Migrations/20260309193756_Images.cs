140efb832235d7b38b0381f47b1930c6c0bdafb2
begsense <begsense@gmail.com>
2026-03-09 23:39:09 +0400

    add migration cuz images

---
+++ Migrations/20260309193756_Images.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hei_Hei_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Images : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Heroes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Animators",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Heroes");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Animators");
+        }
+    }
+}
